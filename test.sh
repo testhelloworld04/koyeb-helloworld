1c59179c5a081da3b024dbe45be0745f7c0acd42
testhelloworld04 <testhelloworld04@gmail.com>
2025-08-24 15:34:42 +0800

    update,

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -ex
+
+# curl -x http://joyous-rosetta-testhelloworld04-ce1a5bce.koyeb.app/test_proxy http://example.com
+curl -x http://joyous-rosetta-testhelloworld04-ce1a5bce.koyeb.app/test_proxy http://ifconfig.me
